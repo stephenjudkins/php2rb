7d9a1278dd4fce8d41d5b896db2db1a47e29412d
Stephen Judkins <stephen@aboutus.org>
2008-10-21 00:13:31 -0700

    added automagical integration specs

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,20 +1,31 @@
 require File.dirname(__FILE__) + '/spec_helper'
+require 'tempfile'
 
-describe Converter, " converts entire scripts" do
+describe Converter, "runs real PHP scripts correctly" do
   before do
     @converter = Converter.new
   end
   after do
     @converter = nil
   end
-  it "converts entire scripts" do
-    xml = File.read(File.dirname(__FILE__) + '/full_script.xml')
-    @converter.eval(e(xml)).should == 'foo = true
-if foo
-print("yes!")
-else
-print("no!")
-end'
-    
+  Dir.glob(File.dirname(__FILE__) + '/integration/*.php').each do |filename|
+    php = File.read(filename)
+    pipe = IO.popen('phc --dump-ast-xml', 'w+')
+    pipe.puts php
+    pipe.close_write
+
+    xml = pipe.readlines.join("\n")
+
+
+    it "should run #{filename}" do
+      ruby = @converter.convert(xml)
+      tmp = Tempfile.new "test_script.rb"
+      tmp.puts ruby
+      tmp.close
+
+      ruby_output = `ruby #{tmp.path}`
+      php_output = `php #{filename}`
+      php_output.should == ruby_output
+    end
   end
 end
---
+++ spec/script_spec.rb
@@ -0,0 +1,20 @@
+require File.dirname(__FILE__) + '/spec_helper'
+
+describe Converter, " converts entire scripts" do
+  before do
+    @converter = Converter.new
+  end
+  after do
+    @converter = nil
+  end
+  it "converts entire scripts" do
+    xml = File.read(File.dirname(__FILE__) + '/full_script.xml')
+    @converter.eval(e(xml)).should == 'foo = true
+if foo
+print("yes!")
+else
+print("no!")
+end'
+    
+  end
+end
